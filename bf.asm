d74e22acbd7f1cbfc4a1016107b30fa724c04460
SidStuff <sidstuffhere@gmail.com>
2024-03-31 00:18:31 +0530

    Fixes and improvements

--- bf.asm
+++ bf.asm
@@ -1,202 +1,159 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;       Brainfuck  Interpreter    ;;
-;;        written by chocabloc     ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Memory Map (relative to PROG_BUFF):
-;
-; 0x0000 to 0x4000: Input Buffer
-; 0x4000 to 0x8000: Preprocessor
-; 0x8000 to 0x10000: Cells
+section .text
+global _start
 
-BITS 64
-ORG 0x10000
-jmp entry
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;        Program Data          ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-LOAD_ADDR     equ 0x10000
-SYSCALL_READ  equ 0	
-SYSCALL_WRITE equ 1
-SYSCALL_BRK   equ 12
-SYSCALL_EXIT  equ 60
-
-STDIN  equ 0
-STDOUT equ 1
-
-PROG_BUFF        equ 0x100000
-BUFF_MAX_SIZE    equ 0x4000
-PP_BUFF_MAX_SIZE equ 0x4000 * 8
-PP_BUFF          equ PROG_BUFF + PP_BUFF_MAX_SIZE
-CELLS            equ PP_BUFF + BUFF_MAX_SIZE
+BUFF_MAX_SIZE    equ 0x10000
+PP_BUFF_MAX_SIZE equ BUFF_MAX_SIZE * 8
+PP_BUFF          equ PROG_BUFF + BUFF_MAX_SIZE
+CELLS            equ PP_BUFF + PP_BUFF_MAX_SIZE
 
 strWelcome:    db "Brainfuck Interpreter v1.0", 10, 10
-strWelcomeLen equ 28
+strWelcomeLen equ $-strWelcome
 strPrompt:     db "bf> "
-strPromptLen  equ 4
+strPromptLen  equ $-strPrompt
 
-entry:
-	; say hello
-	mov rax, SYSCALL_WRITE
-	mov rdi, STDOUT
-	mov rsi, strWelcome
-	mov rdx, strWelcomeLen
-	syscall
+_start:
 
-	; the main loop
-	mainLoop: 
+xor ebp, ebp   ; cell pointer
+
+cmp qword [rsp], 1
+je repl
+
+; open file
+mov rax, 2
+mov rdi, [rsp + 16]
+xor esi, esi
+syscall
+mov rdi, rax
+jmp readInput
+
+repl:
+
+; say hello
+mov rax, 1
+mov rdi, 1
+mov rsi, strWelcome
+mov rdx, strWelcomeLen
+syscall
+
+mainLoop:
 	
 	; print prompt
-	mov rax, SYSCALL_WRITE
-	mov rdi, STDOUT
+	mov rax, 1
+	mov rdi, 1
 	mov rsi, strPrompt
 	mov rdx, strPromptLen
 	syscall
 	
-	; get input
-	mov rax, SYSCALL_READ
-	mov rdi, STDIN
+	xor edi, edi
+	readInput:
+	xor eax, eax
 	mov rsi, PROG_BUFF
 	mov rdx, BUFF_MAX_SIZE
 	syscall
-	mov r15, rax
-	cmp rax, 0
+	
+	cmp rax, 1
 	je exit
+	mov r8, rax
 	
 	; preprocess program
 	; go backwards
-	mov rdi, r15
-	dec rdi
-	ppLoopBk:
-		cmp byte [rdi + PROG_BUFF], ']'
+	lea rbx, [r8 - 1]
+	ppLoop:
+		cmp byte [PROG_BUFF + rbx], ']'
 		jne .nst
-		push rdi
+		push rbx
+		jmp .nnd
 		.nst:
-		cmp byte [rdi + PROG_BUFF], '['
+		cmp byte [PROG_BUFF + rbx], '['
 		jne .nnd
-		pop rdx
-		mov rax, rdi
-		shl rax, 3
-		mov qword [rax + PP_BUFF], rdx
+		pop rsi
+		mov qword [PP_BUFF + rbx*8], rsi
 		.nnd:
-		dec rdi
-		cmp rdi, -1
-		jne ppLoopBk
+		dec rbx
+		jns ppLoop
 	
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	; Start execution of program ;
-	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	; clear memory
-	mov al, 0
-	mov rcx, 0x8000
-	mov rdi, CELLS
-	rep stosb
+	xor ebx, ebx    ; instruction pointer
+	mov rdx, 1      ; bytes to read/write
 	
-	; execute
-	xor rdi, rdi    ; instruction pointer
-	xor rbx, rbx    ; cell pointer
 	execLoop:
-		mov cl, [rdi + PROG_BUFF]
+		
+		mov cl, [PROG_BUFF + rbx]
 		
 		cmp cl, '>'
 		jne .notRight
-		inc rbx
-		jmp .elEnd
+		inc bp
+		jmp .next
 		
 		.notRight:
 		cmp cl, '<'
 		jne .notLeft
-		dec rbx
-		jmp .elEnd
+		dec bp
+		jmp .next
 		
 		.notLeft:
 		cmp cl, '+'
 		jne .notInc
-		inc byte [rbx + CELLS]
-		jmp .elEnd
+		inc byte [CELLS + rbp]
+		jmp .next
 		
 		.notInc:
 		cmp cl, '-'
 		jne .notDec
-		dec byte [rbx + CELLS]
-		jmp .elEnd
+		dec byte [CELLS + rbp]
+		jmp .next
 		
 		.notDec:
 		cmp cl, '.'
 		jne .notPrt
-		mov rsi, rbx
-		call putchar
-		jmp .elEnd
+		mov rax, 1
+		mov rdi, 1
+		lea rsi, [CELLS + rbp]
+		syscall
+		jmp .next
 		
 		.notPrt:
 		cmp cl, ','
 		jne .notInp
-		mov rsi, rbx
-		call getchar
-		jmp .elEnd
+		xor eax, eax
+		xor edi, edi
+		lea rsi, [CELLS + rbp]
+		syscall
+		jmp .next
 		
 		.notInp:
 		cmp cl, '['
 		jne .notLBR
-		cmp byte [rbx + CELLS], 0
-		jne .notLBRZ
-		mov rax, rdi
-		shl rax, 3
-		mov rdi, [rax + PP_BUFF]
-		jmp .elEnd
-		.notLBRZ:
-		push rdi
-		jmp .elEnd
+		cmp byte [CELLS + rbp], 0
+		jne .notZero
+		mov rbx, [PP_BUFF + rbx*8]
+		jmp .next
+		.notZero:
+		push rbx
+		jmp .next
 		
 		.notLBR:
 		cmp cl, ']'
-		jne .elEnd
-		cmp byte [rbx + CELLS], 0
-		je .yesLBRZ
-		mov rdi, [rsp]
-		jmp .elEnd
-		.yesLBRZ:
-		pop rax
+		jne .next
+		cmp byte [CELLS + rbp], 0
+		je .isZero
+		mov rbx, [rsp]
+		jmp .next
+		.isZero:
+		pop rsi
 		
-		.elEnd:
-		inc rdi
-		cmp rdi, r15
+		.next:
+		inc rbx
+		cmp rbx, r8
 		jl execLoop
 	
-	jmp mainLoop
-	
-	exit:
-	mov rax, SYSCALL_EXIT
-	xor rdi, rdi
-	syscall
+	cmp qword [rsp], 1
+	je mainLoop
 
-; pointer to char should be in rsi
-putchar:
-	push rax
-	push rdi
-	push rdx
-	mov rax, SYSCALL_WRITE
-	mov rdi, STDOUT
-	add rsi, CELLS
-	mov rdx, 1
-	syscall
-	pop rdx
-	pop rdi
-	pop rax
-	ret
+exit:
+mov rax, 60
+xor edi, edi
+syscall
 
-; cell index should be in rsi
-getchar:
-	push rax
-	push rdi
-	push rdx
-	mov rax, SYSCALL_READ
-	mov rdi, STDIN
-	add rsi, CELLS
-	mov rdx, 1
-	syscall
-	pop rdx
-	pop rdi
-	pop rax
-	ret
+section .bss
+PROG_BUFF:
+resb 0xA0000
